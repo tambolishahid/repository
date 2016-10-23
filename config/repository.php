691f345919fd76099ea5b81504badc81bc2b32e7
wangxuan <wangxuan@qgpic.com>
2016-10-23 13:45:35 +0800

    add config

---
+++ config/repository.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Repository Cache Enabled
+    |--------------------------------------------------------------------------
+    |
+    | Repository Cache Enabled or not.
+    |
+     */
+    'cache_enabled'   => env('REPOSITORY_CACHE', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Repository Cache TTL
+    |--------------------------------------------------------------------------
+    |
+    | The TTL value of Repository Cache.
+    |
+     */
+    'cache_ttl'       => env('REPOSITORY_CACHE_TTL', 30),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Repository Cache Tags Enabled
+    |--------------------------------------------------------------------------
+    |
+    | Repository Cache Tags Enabled or not.
+    |
+     */
+    'cache_use_tags'  => env('REPOSITORY_CACHE_TAGS', true),
+
+];
